3b88896fc02d6a2da8ba61c9e248e00b07137584
ebear101 <134553824+ebear101@users.noreply.github.com>
2024-04-20 21:38:42 -0400

    Update getClassPeriod.ino

--- CompleteCode/getClassPeriod.ino
+++ CompleteCode/getClassPeriod.ino
@@ -61,3 +61,61 @@ if(letterDay)//G Day
    return "Not during school hours";
   }
 }
+
+if(!letterDay)//V Day
+{
+  if(hour==8&&min<30)
+  {//daily
+    return "Advisory";
+  }
+  else if(hour==8&&min>=35)
+  {
+    return "Intro to Art";
+  }
+  else if(hour==9&&min<=45)
+  {
+    return "Intro to Art";
+  }
+  else if(hour==9&&min>=50)
+  {
+    return "Adapted Physical Education";
+  }
+  else if(hour==10)
+  {
+    return "Adapted Physical Education";
+  }
+  else if(hour==11&&min<=35)
+  {
+    return "Lunch";
+  }
+  else if(hour==11&&min>35)
+  {
+    return "Learn";
+  }
+  else if(hour==12&&min<=10)
+  {
+    return "Learn";
+  }
+  else if(hour==12&&min>10)
+  {
+    return "Appreciation of Pa";
+  }
+  else if(hour==13&&min<=20)
+  {
+   return "Appreciation of Pa";
+  }
+  else if(hour==13&&min>=25)
+  {
+    return "Career Readiness";
+  }
+  else if(hour==14&&min<=35)
+  {
+    return "Between periods";
+  }
+  else
+  {
+    return "Not during school hours";
+  }
+}
+}
+
