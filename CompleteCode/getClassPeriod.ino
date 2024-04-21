4b521ae856203b5bfe9941e8e584a84c944f215c
ebear101 <134553824+ebear101@users.noreply.github.com>
2024-04-20 21:38:16 -0400

    Create getClassPeriod.ino

---
+++ CompleteCode/getClassPeriod.ino
@@ -0,0 +1,63 @@
+//Given the time/date, determines the current class period according to schedule
+
+String getClassPeriod(bool letterDay, int hour, int min)
+{
+if(letterDay)//G Day
+{
+  if(hour==8&&min<=30)
+  {//daily
+    return "Advisory";
+  }
+  else if(hour==8&&min>=35)
+  {
+    return "Concert Choir";
+  }
+  else if(hour==9&&min<=45)
+  {
+    return "Concert Choir";
+  }
+  else if(hour==9&&min>=50)
+  {
+    return "Appreciation of Physical Science";
+  }
+  else if(hour==10)
+  {
+    return "Appreciation of Physical Science";
+  }
+  else if(hour==11&&min<=35)
+  {
+    return "Lunch";
+  }
+  else if(hour==11&&min>35)
+  {
+    return "Learn";
+  }
+  else if(hour==12&&min<=10)
+  {
+    return "Learn";
+  }
+  else if(hour==12&&min>10)
+  {
+    return "Reading";
+  }
+      else if(hour==13&&min<=20)
+  {
+   return "Reading";
+  }
+  else if(hour==13&&min>=25)
+  {
+   return "Math";
+  }
+  else if(hour==14&&min<=35)
+  {
+    return "Math";
+  }
+  else if(hour>8&&hour<15)
+  {
+    return "Between periods";
+  }
+  else
+  {
+   return "Not during school hours";
+  }
+}
